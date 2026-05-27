92c8f3cc7410abff7a1cf5b129b2cc851fbab88d
MPF <pfmaxins@gmail.com>
2026-05-27 14:40:18 -0300

    Added autosave file and QoL configs, mostly for Go

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,11 @@
+return {
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle", -- optional for lazy loading
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading
+  opts = {
+    -- your configuration goes here, or leave empty for defaults
+    execution_message = {
+      enabled = false, -- uncomment to disable saving notifications
+    },
+  },
+}
